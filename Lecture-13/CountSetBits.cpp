8032070d90451b55ebce090d75a46b216c8779cd
Kartik Mathur <kartikmathur@Kartiks-MacBook-Pro.local>
2021-03-05 20:08:23 +0530

    CountSetBits

---
+++ Lecture-13/CountSetBits.cpp
@@ -0,0 +1,39 @@
+// CountSetBits.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	int count_setbits =  0, count_unset = 0;
+
+	while (n != 0) {
+		if ((n & 1) == 1) {
+			count_setbits++;
+		}
+		else {
+			count_unset++;
+		}
+
+		n = n >> 1;
+	}
+
+	cout << "Sets Bits: " << count_setbits << endl;
+	cout << "UnSets Bits: " << count_unset << endl;
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
