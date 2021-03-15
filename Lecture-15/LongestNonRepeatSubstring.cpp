7e478f07262c6c380ae6ae3924c75baf80300f7c
Kartik Mathur <kartikmathur@Kartiks-MacBook-Pro.local>
2021-03-15 21:31:04 +0530

    LongestNonRepeating Substring

--- Lecture-15/LongestNonRepeatSubstring.cpp
+++ Lecture-15/LongestNonRepeatSubstring.cpp
@@ -13,12 +13,12 @@ bool isValid(int *freq) {
 }
 
 int solve(char* a) {
+	// Example: CcodingbLlock, output: CcodingbLl(10), C and c are not considered as same
 	int i = 0, j = 0;
 	int freq[256] = {0};
 	int ans = 0;
 	while (a[j] != '\0') {
 		freq[a[j]]++; // a[j] -> 'a':97, 'A':65
-
 		if (isValid(freq)) {
 			ans = max(ans, j - i + 1);
 		}
@@ -33,12 +33,41 @@ int solve(char* a) {
 	return ans;
 }
 
+int solve1(char* a) { // Here upper case and lower case doesnt matter
+	// Example: CcodingbLlock, output: codingbL(8), C and c are considered as same
+	int i = 0, j = 0;
+	int freq[256] = {0};
+	int ans = 0;
+	while (a[j] != '\0') {
+		if (a[j] >= 'A' and a[j] <= 'Z') {
+			a[j] = a[j] ^ 32; // a[j] = a[j] ^ ' ';
+		}
+		freq[a[j]]++; // a[j] -> 'a':97, 'A':65
+		if (isValid(freq)) {
+			ans = max(ans, j - i + 1);
+		}
+		else {
+			while (isValid(freq) != true) {
+				freq[a[i++]]--;
+			}
+		}
+		j++;
+	}
+
+	return ans;
+}
+
+
 int main() {
 
 	char a[100];
 	cin >> a;
+	for (int i = 0 ; a[i] != '\0' ; i++) {
+		cout << (char)(a[i] ^ ' ');
+	}
+	cout << endl;
+	// cout << solve(a) << endl;
 
-	cout << solve(a) << endl;
 
 	return 0;
 }
