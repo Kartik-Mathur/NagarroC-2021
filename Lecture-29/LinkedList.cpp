8d9903f74083911d224d2394eca8fb73f816dd57
Kartik Mathur <kartik.mathur@codingblocks.com>
2021-04-15 21:34:44 +0530

    Lecture-29 LinkedList

--- Lecture-29/LinkedList.cpp
+++ Lecture-29/LinkedList.cpp
@@ -13,6 +13,15 @@ public:
 	}
 };
 
+int lengthLL(node* head) {
+	int cnt = 0;
+	while (head) {
+		cnt++;
+		head = head->next;
+	}
+	return cnt;
+}
+
 void insertAtFront(node* &head, node* &tail, int d) {
 	node* n = new node(d);
 	if (head == NULL) {
@@ -25,13 +34,45 @@ void insertAtFront(node* &head, node* &tail, int d) {
 	}
 }
 
+void insertAtEnd(node* &head, node* &tail, int d) {
+	node* n = new node(d);
+	if (head == NULL) {
+		head = tail = n;
+	}
+	else {
+		tail->next = n;
+		tail = n;
+	}
+}
+
+void insertAtMid(node* &head, node* &tail, int d, int pos) {
+
+	if (pos == 0) {
+		insertAtFront(head, tail, d);
+	}
+	else if (pos >= lengthLL(head)) {
+		insertAtEnd(head, tail, d);
+	}
+	else {
+		node* temp = head;
+		for (int i = 1; i < pos ; i++) {
+			temp = temp->next;
+		}
+		node* n = new node(d);
+		n->next = temp->next;
+		temp->next = n;
+	}
+}
+
 void print(node* head) {
 	while (head != NULL) {
 		cout << head->data << "-->";
 		head = head->next;
 	}
-	cout << endl;
+	cout << "NULL" << endl;
 }
+
+
 
 int main() {
 
@@ -42,8 +83,14 @@ int main() {
 	insertAtFront(head, tail, 3);
 	insertAtFront(head, tail, 2);
 	insertAtFront(head, tail, 1);
+	insertAtEnd(head, tail, 5);
+	insertAtEnd(head, tail, 6);
+
+
 	print(head);
+	insertAtMid(head, tail, 100, 10);
 	print(head);
+	cout << lengthLL(head) << endl;
 
 	return 0;
 }
